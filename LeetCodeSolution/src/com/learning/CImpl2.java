3ff6e570ce2414784a8a43a76fc13857a84eb4f3
shash <shashi72174@gmail.com>
2020-05-30 21:28:59 +0530

    adding new Impl file for Interface C

---
+++ LeetCodeSolution/src/com/learning/CImpl2.java
@@ -0,0 +1,23 @@
+package com.learning;
+
+public class CImpl2 implements C {
+
+	@Override
+	public void printB() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printA() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printC() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

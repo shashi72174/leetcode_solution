992c74cbaa2fdf2d3e439242a1b939ea06b279f4
shashi72174 <shashi72174@gmail.com>
2020-05-30 14:51:34 +0530

    Update FunctionalInterfaceA.java

--- LeetCodeSolution/src/com/learning/FunctionalInterfaceA.java
+++ LeetCodeSolution/src/com/learning/FunctionalInterfaceA.java
@@ -1,6 +1,6 @@
 package com.learning;
 
-@FunctionalInterface  //this is functional interface, so dont add another abstract method here. it will throw an error
+@FunctionalInterface  //this is functional interface, so dont add another abstract method here. it will throw an error. i will not do boss
 public interface FunctionalInterfaceA {
 	public String singleMeth();
 }

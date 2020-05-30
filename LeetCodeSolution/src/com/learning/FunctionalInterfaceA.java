a77950013a6cea47c55b87d29ac93823b88543e2
shash <shashi72174@gmail.com>
2020-05-30 15:03:05 +0530

    "added new static method in FunctionalinterfaceA

--- LeetCodeSolution/src/com/learning/FunctionalInterfaceA.java
+++ LeetCodeSolution/src/com/learning/FunctionalInterfaceA.java
@@ -3,4 +3,8 @@ package com.learning;
 @FunctionalInterface  //this is functional interface, so dont add another abstract method here. it will throw an error.
 public interface FunctionalInterfaceA {
 	public String singleMeth();
+	
+	public static String singleStaticMeth() {
+		return "singleStaticMeth";
+	}
 }

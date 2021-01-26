4b63653e5bec6d3979f3c160be8a67af6c8af993
shashi72174 <shashi72174@gmail.com>
2021-01-26 15:44:25 +0530

    Update MaximumRepeatingSubstring.java

--- LeetCodeSolution/src/MaximumRepeatingSubstring.java
+++ LeetCodeSolution/src/MaximumRepeatingSubstring.java
@@ -1,16 +1,14 @@
 public class MaximumRepeatingSubstring {
     public static void main(String[] args) {
-        System.out.println(maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba",
-                "aaaba"));
+        System.out.println(maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba","aaaba"));
     }
 
     public static int maxRepeating(String sequence, String word) {
-        int count = 0,i=0;
-        while(i<sequence.length()) {
-            String temp = sequence.substring(0,word.length());
-            if(temp.equals(word))
+        int count = 0;
+        String temp = word;
+        while(sequence.contains(temp)) {
                 count++;
-            sequence = sequence.substring(word.length());
+                temp+=word;
         }
         return count;
     }
